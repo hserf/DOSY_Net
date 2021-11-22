f4ea04119e6762e5008a931ca14106ff78197c37
hserf <ida051124@gmail.com>
2021-11-22 16:26:09 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 15:05:35 2021
+
+@author: Yu
+"""
+
+from train_DOSYEst import train, options
+
+args = options()
+
+idx_case = 2
+
+if idx_case == 1:
+    ## Case 1: simulation
+    #args.input_file = 'data/simulation/testdataSigma0.015.mat'
+    args.input_file = 'data/simulation/testdataSigma0.1.mat'
+    args.output_path = 'Net_Results/sim/'
+    args.diff_range = []
+    args.reg_A = 0.3
+    args.max_iter = 20000
+    args.learning_rate_1 = 1e-4
+    args.learning_rate_2 = 5e-6
+    args.fidelity = 'norm-2'
+    args.n_decay = 3
+    
+elif idx_case == 2:
+    ## Case 2: QGC
+    args.input_file = 'data/QGC/QGC_net_input.mat'
+    args.output_path = 'Net_Results/QGC/'
+    args.diff_range = []
+    #args.diff_range = [3.0, 12.0]
+    args.reg_A = 0.1
+    args.max_iter = 30000
+    args.learning_rate_1 = 2e-4 # 1e-4 is also fine 
+    args.learning_rate_2 = 1e-5 # 5e-6 is also fine but may need a larger "max_iter"
+    args.fidelity = 'norm-2'
+    args.n_decay = 3
+    
+elif idx_case == 3:
+    ## Case 3: GSP
+    args.input_file = 'data/GSP/GSP_net_input.mat'
+    args.output_path = 'Net_Results/GSP/'
+    args.diff_range = []
+    #args.diff_range = [1.0, 6.0]
+    args.reg_A = 0.8
+    args.max_iter = 30000
+    #args.learning_rate_1 = 1e-4
+    #args.learning_rate_2 = 5e-6
+    args.learning_rate_1 = 2e-4 # 1e-4 is also fine 
+    args.learning_rate_2 = 1e-5 # 5e-6 is also fine but may need a larger "max_iter"    
+    args.fidelity = 'norm-2'
+    args.n_decay = 3
+
+elif idx_case == 4:
+    args.input_file = 'data/VD3PVD3/VD3PVD3_net_input.mat'
+    args.output_path = 'Net_Results/VD3PVD3/'
+    args.diff_range = []
+    args.reg_A = 0.2
+    args.max_iter = 30000
+    args.learning_rate_1 = 1e-4
+    args.learning_rate_2 = 5e-6
+    args.fidelity = 'norm-2'
+    args.n_decay = 2
+
+dr, Sp = train(args)
---
+++ model_DOSY_est.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 15:50:59 2021
+
+@author: Yu & Nannan
+
+"""
+
+import tensorflow as tf
+import numpy as np
+
+
+def param_gen_dosy_simple(input_r, n_alphas, n_peaks, t_in, diff_range=[]):
+    with tf.compat.v1.variable_scope('param_gen_dosy_experiment'):
+        conv = tf.keras.layers.Conv1D(filters=16, kernel_size=8, strides=2, activation=tf.nn.leaky_relu)(input_r)
+        conv_flat = tf.reshape(conv, [1, -1])
+        par_a = tf.keras.layers.Dense(units=n_alphas)(conv_flat)
+        par_A = tf.keras.layers.Dense(units=n_alphas*n_peaks)(conv_flat)
+        
+        Ak = tf.reshape(tf.cos(par_A)+1.0, [n_peaks, n_alphas])
+        
+        if not diff_range:
+            z = tf.reshape(tf.nn.sigmoid(par_a), [n_alphas, 1])
+            a = -tf.math.log(z)
+            output, C = harmonic_gen_dosy_z(z, Ak, t_in)
+        else:
+            z_min = np.exp(-diff_range[1])
+            z_max = np.exp(-diff_range[0])
+            tmp = tf.nn.sigmoid(par_a)*(z_max-z_min)+z_min
+            z = tf.reshape(tmp, [n_alphas, 1])
+            a = -tf.math.log(z)
+            output, C = harmonic_gen_dosy_z(z, Ak, t_in)
+        
+        norm_C = tf.square(tf.norm(C, axis=1, keepdims=True))        
+        
+    return a, Ak, output, norm_C
+
+
+def harmonic_gen_dosy_z(z, Ak, t):
+    # size of z: [n_decay, 1]
+    # size of Ak: [n_freq, n_decay]
+    # size of t: [1, n_grad]
+    # size of output: [n_freq, n_grad]
+    C = tf.math.pow(z, t)        
+    output = tf.matmul(Ak, C)
+    return output, C
+
