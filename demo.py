f26d46456744a9a9f4875548122e0572cc4a6aff
hserf <ida051124@gmail.com>
2022-01-19 11:30:02 +0800

    Add files via upload

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ from train_DOSYEst import train, options
 
 args = options()
 
-idx_case = 2
+idx_case = 1
 
 if idx_case == 1:
     ## Case 1: simulation
@@ -27,6 +27,8 @@ if idx_case == 1:
 elif idx_case == 2:
     ## Case 2: QGC
     args.input_file = 'data/QGC/QGC_net_input.mat'
+    #args.input_file = 'data/QGC/QGC.mat'
+    args.threshold = 0.0138
     args.output_path = 'Net_Results/QGC/'
     args.diff_range = []
     #args.diff_range = [3.0, 12.0]
@@ -40,13 +42,13 @@ elif idx_case == 2:
 elif idx_case == 3:
     ## Case 3: GSP
     args.input_file = 'data/GSP/GSP_net_input.mat'
+    #args.input_file = 'data/GSP/GSP.mat'
+    args.threshold = 0.03
     args.output_path = 'Net_Results/GSP/'
     args.diff_range = []
     #args.diff_range = [1.0, 6.0]
     args.reg_A = 0.8
     args.max_iter = 30000
-    #args.learning_rate_1 = 1e-4
-    #args.learning_rate_2 = 5e-6
     args.learning_rate_1 = 2e-4 # 1e-4 is also fine 
     args.learning_rate_2 = 1e-5 # 5e-6 is also fine but may need a larger "max_iter"    
     args.fidelity = 'norm-2'
